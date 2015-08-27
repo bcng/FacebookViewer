302e2e91fedebf90050d2a53d57082ae65bf0f02
Brian Ng <bcn220@gmail.com>
2015-08-27 12:14:04 -0600

    create auth endpoints

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //dependencies
 var express = require('express');
+var bodyParser = require('body-parser');
 var session = require('express-session');
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
@@ -8,20 +9,33 @@ var FacebookStrategy = require('passport-facebook').Strategy;
 var app = express();
 app.use(express.static(__dirname + '/public'));
 
-//include session middleware
+//include session middleware 
 app.use(session({
     secret: 'woot123456'
-}));
+})); //now, our express app is configured with a session with a secret
 
 //include passport middleware
 app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.session()); //now passport knows how to use session
 
 //define Facebook Strategy
 passport.use(new FacebookStrategy({
-    clientID: '<your_client_id>',
-    clientSecret: '<your_client_secret>',
+    clientID: '404071256470186',
+    clientSecret: '39564add7587882300cc73d8c7745a37',
     callbackURL: 'http://localhost:3000/auth/facebook/callback'
 }, function(token, refreshToken, profile, done) {
     return done(null, profile);
 }));
+
+//define auth endpoints
+app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+	successRedirect: '/',
+	failureRedirect: '/login'
+}), function(req, res) {
+	console.log(req.session);
+});
+
+app.listen(3000, function() {
+
+});
